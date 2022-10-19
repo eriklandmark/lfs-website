7af4c98588b9cacb600932dd9045c64ee887a344
erikl <erik@exeri.se>
2022-10-19 15:27:36 +0200

    added i18n

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,15 @@ const {defineConfig} = require('@vue/cli-service')
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+
+    pluginOptions: {
+        i18n: {
+            locale: 'sv',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false,
+            enableBridge: false
+        }
+    }
 })
