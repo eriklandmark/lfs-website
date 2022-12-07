371b808fb7f54f09ed939569f709d0d5ed3079c5
erikl <erik@exeri.se>
2022-12-07 22:40:47 +0100

    better design

--- src/views/NotAuthView.vue
+++ src/views/NotAuthView.vue
@@ -4,8 +4,8 @@
             v-row.my-16(align="center" justify="center")
                 v-col(cols="12" md="7" style="text-align: center")
                     v-icon(color="error" size="70") mdi-alert-circle
-                    h1.mt-4.mb-2 {{$t('error_codes.404.title')}}
-                    span {{$t('error_codes.404.description')}}
+                    h1.mt-4.mb-2 {{$t('error_codes.403.title')}}
+                    span {{$t('error_codes.403.description')}}
 
 </template>
 
@@ -15,5 +15,5 @@ import {Component, Vue} from 'vue-property-decorator';
 @Component({
     components: {}
 })
-export default class NotFoundView extends Vue {}
+export default class NotAuthView extends Vue {}
 </script>
