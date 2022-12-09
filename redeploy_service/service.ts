34c4f73bf407747aa6048bf0bf7c752624d3167b
erikl <erik@exeri.se>
2022-12-09 14:24:46 +0100

    added a redepoly service

---
+++ redeploy_service/service.ts
@@ -0,0 +1,21 @@
+const http = require("http");
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
+const service = process.env.REDEPLOY_SERVICE;
+
+const server = http.createServer(async (req: any, res: any) => {
+    try {
+        console.log("Received request to update service");
+        await exec("cd repo && git pull")
+        await exec(`cd repo && docker compose down ${service}`)
+        await exec(`cd repo && docker compose up -d --build ${service}`)
+        console.log("Request processed");
+    } catch (error) {
+        console.error(error);
+    }
+    res.writeHead(200);
+    res.end("");
+});
+server.listen(3000, "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
