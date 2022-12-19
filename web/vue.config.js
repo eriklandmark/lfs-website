b0b4d06c792cbda415c69f076ffbd2937ef778af
erikl <erik@exeri.se>
2022-12-19 21:48:16 +0100

    removed icon paths

--- web/vue.config.js
+++ web/vue.config.js
@@ -13,5 +13,18 @@ module.exports = defineConfig({
             enableInSFC: false,
             enableBridge: false
         }
+    },
+    pwa: {
+        name: 'LFS Website',
+        themeColor: '#1E90FF',
+        msTileColor: '#032040',
+        appleMobileWebAppCapable: 'yes',
+        iconPaths: {
+            favicon32: null,//'img/icons/favicon-32x32.png',
+            favicon16: null,//'img/icons/favicon-16x16.png',
+            appleTouchIcon: null,//'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: null,//'img/icons/safari-pinned-tab.svg',
+            msTileImage: null,//'img/icons/msapplication-icon-144x144.png'
+        }
     }
 })
