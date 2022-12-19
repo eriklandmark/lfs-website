c6d0adc86bce867e4493c27f3faa32a1115e7da6
erikl <erik@exeri.se>
2022-12-19 21:44:10 +0100

    updated scripts to remove orphan containers

--- tools/redeploy_release.sh
+++ tools/redeploy_release.sh
@@ -1,4 +1,4 @@
 cd ..
 docker compose -f docker-compose.release.yml down
 git pull
-docker compose -f docker-compose.release.yml up -d --build
+docker compose -f docker-compose.release.yml up -d --build --remove-orphans
